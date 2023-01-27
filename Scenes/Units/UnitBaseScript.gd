af43846faa68ab1c6f141ffa4ce3763b6d39c758
Erik M Jacobs <erikmjacobs@gmail.com>
2023-01-26 20:33:41 -0500

    gets a base unit, base freighter on the screen

---
+++ Scenes/Units/UnitBaseScript.gd
@@ -0,0 +1,9 @@
+class_name Unit
+extends Node
+
+
+@export var moves_allowed: int
+
+
+func base_setup(base_moves: int = 0):
+  moves_allowed = base_moves
--- Scenes/game.gd
+++ Scenes/game.gd
@@ -5,11 +5,15 @@ var HexGrid = preload("./hex_grid.gd").new()
 var HexWidth = 3543
 var hex_ratio: float
 var current_turn = 0
+var starting_funds = 1000000
 
-@export var grid_size = 4
+@export var grid_size = 2
 @export var hex_scale_size = 50.0
 
+@onready var tile_area = $Tiles
+@onready var unit_area = $Units
 @onready var hex_tile = preload("res://Scenes/hex_tile.tscn")
+@onready var base_freighter = preload("res://Scenes/Units/BaseFreighter.tscn")
 @onready var end_turn_button = $UserInterface/BottomUI/HBoxContainer/Button
 @onready var turn_counter = $UserInterface/TopUI/HBoxContainer/TurnCounter
 
@@ -25,6 +29,9 @@ func _ready():
 
 	# build the hex grid
 	_build_hex_grid()
+
+	# place the base freighter
+	_place_initial_freighter()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -46,12 +53,35 @@ func _place_tile(x,y):
 	# instantiate a hex tile at the hex center
 	var current_hex_tile = hex_tile.instantiate()
 
+  # store the hex coordinates of the tile
 	current_hex_tile.local_hex_coords = Vector2(x,y)
 
+  # scale the artwork
 	current_hex_tile.apply_scale(Vector2(hex_ratio, hex_ratio))
-	current_hex_tile.position = current_hex_center + Vector2(960,540)
 
-	add_child(current_hex_tile)
+  # place the tile
+	current_hex_tile.position = current_hex_center
+
+  # name the tile and add it to the tiles group
+	current_hex_tile.name = "Hex: " + str(x) + "," + str(y)
+	current_hex_tile.add_to_group("hex_tiles")
+
+  # add the tile to the scene
+	tile_area.add_child(current_hex_tile)
+
+func _place_initial_freighter():
+	var initial_freighter = base_freighter.instantiate()
+	initial_freighter.base_setup(5)
+
+	var zero_hex = HexGrid.get_hex_center(Vector2(0,0))
+	initial_freighter.position = zero_hex
+	initial_freighter.apply_scale(Vector2(0.5, 0.5))
+
+	initial_freighter.add_to_group("units")
+	unit_area.add_child(initial_freighter)
+
+
+# signal handling
 
 func _on_end_turn():
 	print("Game.gd: turn button pressed!")
